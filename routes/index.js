91b1212b967e504247c5080bcd716654cf0d5465
luis <luisconstante@luisconstante.local>
2016-09-09 18:30:06 -0700

    finished layout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var content = '<p class="wow fadeInUp"' + 'data-wow-delay="1s" >' + 'Copyright &copy; 2016 White Eagle General Construction |' + 
+                '<a href="http://luisconstante.com"' + 'title="Awesome Web Development"' + ' target="_blank">' + 'Webmaster</a></p>'
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', footer: content });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express', footer: content });
+});
+
+router.get('/gallery', function(req, res, next) {
+  res.render('gallery', { link: 'Contact Us', footer: content });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express', footer: content });
 });
 
 module.exports = router;
